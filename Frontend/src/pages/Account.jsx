b53f8437684a61a12a912e1255be63576a2b9e3a
조성한 <toor21@naver.com>
2022-05-04 13:52:26 +0900

    feat: 계정 정보 변경

--- Frontend/src/pages/Account.jsx
+++ Frontend/src/pages/Account.jsx
@@ -3,59 +3,100 @@ import { useSelector } from 'react-redux'
 import { Route } from 'react-router-dom'
 import Divider from '@mui/material/Divider'
 import Box from '@mui/material/Box'
+import axios from 'axios'
 
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, IconButton } from '@mui/material'
 import Close from '@mui/icons-material/Close'
 
 const ChangeNickname = (props) => {
   const user = props.user
+  const [input, setInput] = useState(user.nickname)
+  
+  const handleNicknameButton = (event) => {
+    event.preventDefault()
+    console.log(input)
+
+    // console.log들은 snackbar, alert 등으로 바꿀 것
+    if (input === user.nickname) {
+      console.log('동일한 닉네임입니다.')
+      return
+    }
+
+    if (input === '') {
+      console.log('올바르지 않은 닉네임입니다.')
+      return
+    }
+
+    const newuser = {...user, nickname: input}
+    axios({
+      baseURL: process.env.REACT_APP_SERVER_URL,
+      timeout: 3000,
+      headers: {
+        'Authentication': ''
+      },
+      url: '',
+      method: 'PUT',
+      data: newuser
+    })
+    .then(response => {
+      console.log('닉네임이 변경되었습니다.')
+      // 페이지 재로드
+    })
+    .catch(error => {
+      console.log('닉네임 변경에 실패했습니다.')
+      console.log(error)
+    })
+  }
+    
   return (<Box textAlign='start' m={3}>
   <h3>닉네임 변경</h3>
   <p>새로 사용할 닉네임을 작성해주세요.</p>
-  <Box textAlign='center'>
-    <TextField value={user.nickname} sx={{mr: 1}} size='small' />
-    <Button variant='contained' sx={{}}>변경</Button>
+  <Box textAlign='center' sx={{whiteSpace: 'nowrap'}}>
+    <TextField value={input} onChange={(event) => setInput(event.target.value)}  sx={{mr: 1}} size='small' />
+    <Button variant='contained' sx={{}} onClick={handleNicknameButton}>
+      변경
+    </Button>
   </Box>
-  </Box>)
+  </Box>)  
 }
 
+
 const SignOut = (props) => {
+  const [open, setOpen] = useState(false)
+
   return (<Box textAlign='start' m={3}>
     <h3>회원 탈퇴</h3>
     <Box textAlign='center'>
-      <Button variant='contained'>회원 탈퇴</Button>
+      <Button variant='contained' onClick={() => {setOpen(true)}}>회원 탈퇴</Button>
     </Box>
-
+    <SignOutModal open={open} setOpen={setOpen} />
   </Box>)
 }
 
 const SignOutModal = (props) => {
-  return (<Dialog>
+
+  return (<Dialog open={props.open}>
     <DialogTitle>회원탈퇴</DialogTitle>
-    <IconButton edge='end' onClick={{}} sx={{position: 'absolute', right: 20, top: 20}}>
+    <IconButton edge='end' onClick={() => {props.setOpen(false)}} sx={{position: 'absolute', right: 20, top: 20}}>
       <Close />
     </IconButton>
     <DialogContent>
-      <p>정말로 탈퇴하시겠습니까?</p>
+      <p>SSAM에서 탈퇴하시면 더 이상 SSAM이 제공하는 야구 데이터 분석 / 시뮬레이션 정보를 이용하실 수 없습니다. 정말로 탈퇴하시겠습니까?</p>
       <DialogActions>
-        <Button>
+        <Button fullWidth variant="contained">
           탈퇴
         </Button>
       </DialogActions>
       <DialogActions>
-        <Button>
+        <Button onClick={() => {props.setOpen(false)}} variant="contained" fullWidth color="secondary">
           취소
         </Button>
       </DialogActions>
     </DialogContent>
-
-
-
   </Dialog>)
 }
 
 export default function Account() {
-  const [info, setInfo] = useState({})
   const user = useSelector((state) => state.user)
   
   useEffect(() => {
@@ -69,7 +110,7 @@ export default function Account() {
   return (
     <>
       <h1>회원 정보</h1>
-      <ChangeNickname user={info} />
+      <ChangeNickname user={user} />
       <Divider sx={{my: 3}} />
       <SignOut />
     </>
